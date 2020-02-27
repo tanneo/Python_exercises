db25b14cefff52e5ecb1d924c5242d6b9b144ba3
Ruth Oar <ruthoar@Ruths-MacBook-Pro.local>
2020-02-27 16:09:50 +1000

    ex32

---
+++ ex32.py
@@ -0,0 +1,34 @@
+#loops and lists
+#list is a loop of things ordered from first to last
+
+the_count = [1,2,3,4,5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+#This first kind of for loop goes through a list
+
+for number in the_count:
+    print(f"This is count {number}")
+    
+#same as above
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+#notice that we have to us {} since we don't know what is in it
+
+for i in change:
+    print(f"I got {i}")
+
+#We can also build lists. First start with an empty one
+elements = []
+
+#then use the range function to do 0 to 5 counts
+for i in range(0,6):
+    print(f"Adding {i} to the list")
+    #append is a function that lists understand 
+    elements.append(i)
+
+#now we can print them out too
+    for i in elements:
+        print(f"Element was: {i}")
