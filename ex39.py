5e91c9eb9e59ab08bc7e4db43a5b7a888115fe93
Ruth Oar <ruthoar@Ruths-MacBook-Pro.local>
2020-03-02 13:20:07 +1000

    added australian cities to dictionary ex.39

--- ex39.py
+++ ex39.py
@@ -58,3 +58,43 @@ if not state:
 #get a city with a default value
 city = cities.get('TX', 'Does not exist')
 print(f"The city for the state 'TX' is: {city}")
+
+aus_states = {
+    'Queensand': 'QLD',
+    'New South Wales': 'NSW',
+    'Tasmania': 'TAS',
+    'Western Australia': 'WA',
+    }
+
+#create a basic set of states and some cities within those cities
+aus_cities = {
+    'QLD' : 'Brisbane',
+    'NSW' : 'Sydney',
+    'WA' : 'Perth'
+}
+
+#add some more cities
+aus_cities['TAS'] = "Hobart"
+aus_cities['QLD'] = 'Cairns'
+
+#print out some cities
+print(f"QLD has :", aus_cities['QLD'])
+
+#print out some states
+print(f"New South Wales abbreviation is:", aus_states['New South Wales'])
+
+#print every state abbreviation
+for aus_state, abbrev in list(aus_states.items()):
+    print(f"{aus_state} is abbreviated {abbrev}")
+
+#print every city in state
+print('-' * 10)
+for abbrev, city in list(aus_cities.items()):
+    print(f"{abbrev} has the city: {city}")
+
+#now co both at the same time
+print('-' * 10)
+for state, abbrev in list(aus_states.items()):
+    print(f"{aus_state} state is abbreviated {abbrev}")
+    print(f"and has city {aus_cities[abbrev]}")
+
